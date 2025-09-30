52e6acc9353194e71f13a6a29ff0e3354e8d4f4b
duane <duane.nielsen.rocks@gmail.com>
2025-09-29 17:43:07 -0700

    feat: add enhanced ECS export debug output

--- external/madrona/src/core/state.cpp
+++ external/madrona/src/core/state.cpp
@@ -623,8 +623,47 @@ void StateManager::copyOutExportedColumns()
 {
 #ifdef MADRONA_MW_MODE
 
+#ifdef MADRONA_ECS_DEBUG_TRACKING
+    printf("ECS EXPORT: copying %u components\n", (uint32_t)export_jobs_.size());
+#endif
+
     for (ExportJob &export_job : export_jobs_) {
         auto &archetype = *archetype_stores_[export_job.archetypeIdx];
+
+#ifdef MADRONA_ECS_DEBUG_TRACKING
+        // Get component name using ECS debug system
+        const char* component_name = nullptr;
+
+        if (archetype.tblStorage.tbls.size() > 0 && archetype.tblStorage.tbls[0].numRows() > 0) {
+            void* sample_addr = archetype.tblStorage.tbls[0].data(export_job.columnIdx);
+            address_info_t debug_info;
+            if (simple_tracker_lookup(sample_addr, &debug_info)) {
+                component_name = debug_info.component_name;
+            }
+        }
+
+        if (!component_name) {
+            component_name = "Unknown";
+        }
+
+        // Try to get tensor name from export buffer registration
+        const char* tensor_name = simple_tracker_lookup_export_tensor_name(export_job.mem.ptr());
+
+        // Count total rows across all tables
+        CountT total_rows = 0;
+        for (Table &tbl : archetype.tblStorage.tbls) {
+            total_rows += tbl.numRows();
+        }
+
+        if (tensor_name) {
+            printf("  %s -> %s (%p): %u rows, %u bytes/row\n",
+                   component_name, tensor_name, export_job.mem.ptr(),
+                   (uint32_t)total_rows, export_job.numBytesPerRow);
+        } else {
+            printf("  %s: %u rows, %u bytes/row -> %p\n",
+                   component_name, (uint32_t)total_rows, export_job.numBytesPerRow, export_job.mem.ptr());
+        }
+#endif
 
         CountT cumulative_copied_rows = 0;
         for (Table &tbl : archetype.tblStorage.tbls) {
